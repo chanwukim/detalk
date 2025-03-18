e5af492494d56d758c65a99e2bfa34b9abfd9968
chanwukim <chnwu.kim@gmail.com>
2025-03-18 10:04:09 +0900

    feat: Toast

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,34 @@
 import { QueryClientProvider } from "@/libs/query";
+import { Toaster } from "@/libs/toast";
 
 export default function Providers({ children }: React.PropsWithChildren) {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider>
+      {children}
+      <Toaster
+        position="top-center"
+        duration={3500}
+        toastOptions={{
+          className:
+            "text-foreground border border-transparent dark:border-border",
+          success: {
+            className:
+              "[&_[data-icon]]:text-green-500 dark:[&_[data-icon]]:text-green-400",
+          },
+          info: {
+            className:
+              "[&_[data-icon]]:text-blue-500 dark:[&_[data-icon]]:text-blue-400",
+          },
+          warning: {
+            className:
+              "[&_[data-icon]]:text-yellow-500 dark:[&_[data-icon]]:text-yellow-400",
+          },
+          error: {
+            className:
+              "[&_[data-icon]]:text-red-500 dark:[&_[data-icon]]:text-red-400",
+          },
+        }}
+      />
+    </QueryClientProvider>
+  );
 }
